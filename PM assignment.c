13f3cf9cbcc465c8d57a64a67fb449400a0af43e
WageeshaEdirisinghe <it25102771@my.sliit.lk>
2025-09-27 16:43:29 +0530

    tic-tac-toe

--- PM assignment.c
+++ PM assignment.c
@@ -1,9 +1,12 @@
-// Online C compiler to run C program online
+//player vs player
+
 #include<stdio.h>
 #include <stdlib.h>
+#include <time.h> 
 
+ // initialize the board..............................................
 char** initialize_the_Board(int N) {
-    // initialize the board..............................................
+   
     char** board = malloc(N * sizeof(char*));
     for (int i = 0; i < N; i++) {
         board[i] = malloc(N * sizeof(char));
@@ -22,17 +25,10 @@ void display_the_board ( char** board, int N){
         }
          printf("\n");
 
-    if (i != N - 1) {
-            for (int k = 0; k < N; k++) {
-                printf("---\n");
-
-                if (k != N - 1) printf("+\n");
-            }
-            printf("");
-        }
+   
     }
 }
-
+//win.......................................................................
 int checkWin(char** board, int N, char sym) {
     
     for (int i = 0; i < N; i++) {
@@ -54,7 +50,7 @@ int checkWin(char** board, int N, char sym) {
     return 0;
 }
 
-// Check for draw
+// draw....................................................................
 int isDraw(char** board, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -62,6 +58,16 @@ int isDraw(char** board, int N) {
     }
     return 1; 
     }
+}
+// Computer
+void computer(char** board, int N) {
+    int r, c;
+    do {
+        r = rand() % N;
+        c = rand() % N;
+    } while (board[r][c] != '-');
+    board[r][c] = 'O';
+    printf("Computer placed O at (%d, %d)\n", r + 1, c + 1);
 }
 
 int main() {
@@ -73,35 +79,50 @@ int main() {
         printf("Invalid size!\n");
         return 1;
     }
+    int mode;
+    printf(" mode: 1 = Player vs Player, 2 = Player vs Computer: ");
+    scanf("%d", &mode);
+    if (mode != 1 && mode != 2) {
+        printf("Invalid mode!\n");
+        return 1;
+    }
 
-    char **board = createBoard(N);
+    char **board = initialize_the_Board(N);
+    srand(time(NULL)); //randomness
 
     int turn = 0; // 0 = X, 1 = O
     while (1) {
-        showBoard(board, N);
+        display_the_board(board, N);
         int r, c;
         char sym = (turn == 0) ? 'X' : 'O';
-
-        printf("Player %c, enter row and column (1-%d): ", sym, N);
-        scanf("%d %d", &r, &c);
-        r--; c--; 
-        if (r < 0 || r >= N || c < 0 || c >= N || board[r][c] != '-') {
-            printf("Invalid move! Try again.\n");
-            continue;
+        
+           if (mode == 1 || (mode == 2 && turn == 0)) {
+            // Human
+            printf("Player %c, enter row[space]column (1-%d): ", sym, N);
+            scanf("%d %d", &r, &c);
+            r--; c--;
+            if (r < 0 || r >= N || c < 0 || c >= N || board[r][c] != '-') {
+                printf("Invalid move! Try again.\n");
+                continue;
+            }
+            board[r][c] = sym;
+        } else {
+            // Computer
+            computer(board, N);
         }
 
-        board[r][c] = sym;
-
         if (checkWin(board, N, sym)) {
-            showBoard(board, N);
+            display_the_board(board, N);
             printf("Player %c wins!\n", sym);
             break;
         }
         if (isDraw(board, N)) {
-            showBoard(board, N);
+            display_the_board(board, N);
             printf("It's a draw!\n");
             break;
         }
+        
+       
 
         turn = 1 - turn; // switch player
     }
@@ -112,6 +133,9 @@ int main() {
 
     return 0;
 }
+
+
+
 
 
 
